1d9d971377e08efae31e4c9111443bef68c704ef
coleyrockin <coleyrockin@aol.com>
2022-08-01 22:35:50 -0500

    small change

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,13 +1,12 @@
-// using Manager constructor 
 const Manager = require('../lib/Manager');
 
-// creating manager object  
+// Creating manager object  
 test('creates an Manager object', () => {
     const manager = new Manager('Boyd Roberts', 90, 'coleyrockin@gmail', 4);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
-// gets role from getRole()
+// Gets role
 test('gets role of employee', () => {
     const manager = new Manager('Boyd', 90, 'coleyrockin@gmail.com');
     expect(manager.getRole()).toEqual("Manager");
